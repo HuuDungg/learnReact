45ea55343b976dbe22125dcbee00d87ed0dbc8e2
HuuDungg <huudung2004kt@gmail.com>
2024-07-31 16:31:39 +0700

    fragment and css inline in react component

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,6 +1,12 @@
+import './style.css';
 const MyComponent = () => {
     return (
+        <>
+        <div>
+            <h1 style={{color: 'red'}}>Hi there. My name is</h1> <h1 className='name'>Tran Huu Dung</h1>
+        </div>
         <div>Hi there. My name is Tran Huu Dung</div>
+        </>
     );
 }
 
