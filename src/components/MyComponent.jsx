c3dc072946ac429fc029ab7e1a0d3b898f87e57a
HuuDungg <huudung2004kt@gmail.com>
2024-07-31 16:45:12 +0700

    variable in JSX

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,11 +1,16 @@
 import './style.css';
+const Dog = {
+    name: "gau gau",
+    descrioption: "over loyal"
+}
+const name = "Tran Huu Dung hihih"
 const MyComponent = () => {
     return (
         <>
         <div>
-            <h1 style={{color: 'red'}}>Hi there. My name is</h1> <h1 className='name'>Tran Huu Dung</h1>
+            <h1 style={{color: 'red'}}>Hi there. My name is</h1> <h1 className='name'>{name}</h1>
+            <h2>{JSON.stringify(Dog)}</h2>
         </div>
-        <div>Hi there. My name is Tran Huu Dung</div>
         </>
     );
 }
