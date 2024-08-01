204c7841e74a81385f8dd1088d40ed2eb19fc710
HuuDungg <huudung2004kt@gmail.com>
2024-08-01 15:58:29 +0700

    client router with link

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,25 +1,19 @@
+import { Link } from "react-router-dom"
 const Header = () =>{
     return(
         <>
-            <nav class="navbar navbar-inverse">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                    <a class="navbar-brand" href="#">WebSiteName</a>
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                    <a className="navbar-brand" href="#">WebSiteName</a>
                     </div>
-                    <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                        <li><a href="#">Page 1-1</a></li>
-                        <li><a href="#">Page 1-2</a></li>
-                        <li><a href="#">Page 1-3</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Page 2</a></li>
+                    <ul className="nav navbar-nav">
+                        <li ><Link className="active" to='/home' >Home</Link></li>
+                        <li ><Link className="active" to='/users' >user</Link></li>
                     </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
                     </ul>
                 </div>
             </nav>
